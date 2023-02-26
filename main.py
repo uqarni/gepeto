9b5f5beed1087eea2b39f51ad5de9d66b8c35e8a
Uzair Qarni <108438834+uqarni@users.noreply.github.com>
2023-02-26 04:00:00 +0000

    index with drag and drop but idk how it would work

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return 'Hello from Flask!'
+  return render_template('index.html')
 
 
 app.run(host='0.0.0.0', port=81)
